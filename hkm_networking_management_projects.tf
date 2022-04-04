185f303a0f5b9feef4d4998f6c5b66ff5ed15dde
Jesper Bergeskans <jesperbergeskans@devoteam.com>
2022-04-04 10:43:50 +0200

    Removing cyclic error

--- hkm_networking_management_projects.tf
+++ hkm_networking_management_projects.tf
@@ -4,6 +4,4 @@ resource "google_project" "pj-data-team" {
     folder_id       = var.root_folder_id
     name            = "Data team network host"
     billing_account = var.billing_account
-
-    depends_on = [ google_project_service.cc_host_rm_api ]
 }
